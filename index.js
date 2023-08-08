6229f306f31a6072d92a473ad32dbbc56b6f837c
ShrujanaReddy <130744023+ShrujanaReddy@users.noreply.github.com>
2023-08-08 20:12:19 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,38 @@
+const form = document.getElementById('registrationForm');
+        const userTable = document.getElementById('userTable');
+
+        form.addEventListener('submit', function (event) {
+            event.preventDefault();
+
+            const name = document.getElementById('name').value;
+            const email = document.getElementById('email').value;
+            const dob = document.getElementById('dob').value;
+            const password=document.getElementById('password').value;
+            //const acceptTerms=document.getElementById('acceptTerms').value;
+
+            const userAge = calculateAge(new Date(dob));
+
+            if (userAge < 18 || userAge > 55) {
+            alert('Age must be between 18 and 55.');
+            return false; // Prevent form submission
+            }
+
+            const user = { name, email, dob, password/*,acceptTerms */};
+
+            // Store data in local storage
+            let users = JSON.parse(localStorage.getItem('users')) || [];
+            users.push(user);
+            localStorage.setItem('users', JSON.stringify(users));
+
+            // Add user data to table
+            const newRow = userTable.insertRow();
+            newRow.innerHTML = `
+                <td class="border p-2 justify-center">${user.name}</td>
+                <td class="border p-2 justify-center">${user.email}</td>
+                <td class="border p-2 justify-center">${user.password}</td>
+                <td class="border p-2 justify-center">${user.dob}</td>
+                <td class="border p-2 justify-center">true</td>
+            `;
+
+            form.reset();
+        });
